25d22fb7984935c0864d35b5c4d524cac966522f
Garrett Bowman <garrettbowman248@gmail.com>
2024-04-17 17:46:16 -0500

    Made corrections to classes

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class StatTracker
     
     def initialize(teams, games, game_teams)
@@ -10,11 +12,11 @@ class StatTracker
         games = generate_csv_data(data[:games])
         teams = generate_csv_data(data[:teams])
         game_teams = generate_csv_data(data[:game_teams])
+
         StatTracker.new(teams, games, game_teams)
     end
 
     def self.generate_csv_data(data)
-
         CSV.foreach(data, headers: true) do |row|
             if data == './data/games.csv'
                 Games.new(row)
@@ -27,13 +29,8 @@ class StatTracker
     end
 
     def highest_total_score
-        total_scores = []
-        @games.all_games.each do |game|
-            game_total = game.away_goals.to_i + game.home_goals.to_i
-            total_scores << game_total
-            
-        end
-
-        total_scores
+        @games.all_games.map do |game|
+            game.away_goals + game.home_goals
+        end.max
     end
 end
